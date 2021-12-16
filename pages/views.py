8d69a558453a1cfc5b51fc281092b8c11d5c0e21
Moritz Moser <moritz@baseofm.de>
2021-12-16 14:23:34 +0100

    Add a landing page

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'pages/index.html')
+
+
+def test(request, company_name):
+    print(company_name)
+    return HttpResponse("Working")
+
