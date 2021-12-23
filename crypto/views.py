46f4c91993090252610c04909467cf488104faff
Moritz Moser <moritz@baseofm.de>
2021-12-23 12:03:36 +0100

    Add style_green.css and style_red.css which will be used depending on positive/negative sentiment_score

--- crypto/views.py
+++ crypto/views.py
@@ -8,5 +8,5 @@ def search_crypto(request):
         searched_crypto = request.POST['searched_crypto']
         use_case = SearchCrypto(searched_crypto)
         combined_analysed_score = use_case.execute()
-        print(combined_analysed_score)
-        return render(request, 'crypto/search_crypto.html', {'searched_crypto': searched_crypto})
+        crypto_data = {'name': searched_crypto, 'score': combined_analysed_score}
+        return render(request, 'crypto/search_crypto.html', {'crypto_data': crypto_data})
