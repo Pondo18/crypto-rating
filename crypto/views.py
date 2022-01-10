107c858f242a9452d7500e140f217cc1277aadbf
Moritz Moser <moritz@baseofm.de>
2022-01-10 14:26:20 +0100

    Redirect user to index page, when searching for a invalid crypto

--- crypto/views.py
+++ crypto/views.py
@@ -1,12 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+import crypto
 from crypto.usecases import SearchCrypto
 
 
 def search_crypto(request):
     if request.method == 'POST':
         searched_crypto = request.POST['searched_crypto']
-        use_case = SearchCrypto(searched_crypto)
-        combined_analysed_score = use_case.execute()
-        crypto_data = {'name': searched_crypto, 'score': combined_analysed_score}
-        return render(request, 'crypto/search_crypto.html', {'crypto_data': crypto_data})
+        use_case = SearchCrypto(searched_crypto.lower())
+        try:
+            combined_analysed_score = use_case.execute()
+            crypto_data = {'name': searched_crypto, 'score': combined_analysed_score}
+            return render(request, 'crypto/search_crypto.html', {'crypto_data': crypto_data})
+        except crypto.models.Currencies.DoesNotExist:
+            # messages.error(request, 'Crypto not found')
+            return redirect('/')
